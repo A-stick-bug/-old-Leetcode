c1bf3887bbc8102bbef013abf91335f6906b61a3
A-stick-bug <104738598+A-stick-bug@users.noreply.github.com>
2023-06-20 15:38:51 -0400

    fixed code

--- 150. Evaluate Reverse Polish Notation.py
+++ 150. Evaluate Reverse Polish Notation.py
@@ -3,20 +3,26 @@
 def evalRPN(tokens) -> int:
     stack = []
     for token in tokens:
-        b = int(stack.pop())
-        a = int(stack.pop())
         if token == "+":
-            stack.append(a+b)
+            b = int(stack.pop())
+            a = int(stack.pop())
+            stack.append(a + b)
         elif token == "-":
-            stack.append(a-b)
+            b = int(stack.pop())
+            a = int(stack.pop())
+            stack.append(a - b)
         elif token == "*":
-            stack.append(a*b)
+            b = int(stack.pop())
+            a = int(stack.pop())
+            stack.append(a * b)
         elif token == "/":
-            stack.append(a/b)  # no need to cast to int because it will be cast when being popped from the stack
+            b = int(stack.pop())
+            a = int(stack.pop())
+            stack.append(a / b)
         else:
             stack.append(token)
 
     return int(stack[0])
 
-
-print(evalRPN(["18"]))
+test = "3 2 1 + - 9 -".split()
+print(evalRPN(test))
